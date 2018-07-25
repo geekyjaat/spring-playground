7dc6e73debe2cd657ddc03ecf103552cfbcfcc94
Gulia, Vikram <vgulia@hawk.iit.edu>
2018-07-25 14:28:16 -0600

    added math pi method

--- src/main/java/com/example/demo/math/MathController.java
+++ src/main/java/com/example/demo/math/MathController.java
@@ -1,16 +1,15 @@
-package com.example.demo;
+package com.example.demo.math;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/questions/bysurvey")
-public class HelloController {
+@RequestMapping("/math")
+public class MathController {
 
-    @GetMapping("/all")
-    public String helloWorld() {
-        return "Hello from Spring!";
+    @GetMapping("/pi")
+    public String pi(){
+        return "3.141592653589793";
     }
-
 }
