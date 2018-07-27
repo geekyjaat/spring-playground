d16241926dac60ef07f4c0f7771d84c7f1665ac3
Gulia, Vikram <vgulia@hawk.iit.edu>
2018-07-27 11:09:05 -0600

    imports optimize

--- src/main/java/com/example/demo/model/Flight.java
+++ src/main/java/com/example/demo/model/Flight.java
@@ -1,9 +1,6 @@
 package com.example.demo.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
