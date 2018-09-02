0258b4b708400f33956fd2982d3d353a288772ba
gulia <vgulia@hawk.iit.edu>
2018-09-01 18:38:10 -0600

    Unit 8, Exercise 05 - completed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,15 +2,21 @@ package com.example.demo;
 
 import com.example.demo.entities.Employee;
 import com.example.demo.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
+
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -26,11 +32,12 @@ public class DemoApplication {
     public CommandLineRunner seedData(EmployeeRepository employeeRepository) {
         return (args) -> {
             employeeRepository.deleteAll();
+
             Employee employee = new Employee();
             employee.setName("Employee");
             employee.setSalary(24);
             employee.setUsername("employee");
-            employee.setPassword("my-employee-password");
+            employee.setPassword(passwordEncoder.encode("my-employee-password"));
             employee.setRole("EMPLOYEE");
             employeeRepository.save(employee);
 
@@ -38,7 +45,7 @@ public class DemoApplication {
             boss.setName("Bossy Boss");
             boss.setSalary(24);
             boss.setUsername("boss");
-            boss.setPassword("my-boss-password");
+            boss.setPassword(passwordEncoder.encode("my-boss-password"));
             boss.setRole("MANAGER");
             employeeRepository.save(boss);
         };
--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,13 +1,14 @@
 package com.example.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -30,7 +31,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(employeeDetailsService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+        auth
+                .userDetailsService(employeeDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
